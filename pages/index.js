f662e77d70c79b4a5304338e152d08af25398dc5
dhe-plot <115636889+dhe-plot@users.noreply.github.com>
2022-10-30 21:21:51 +0100

    edited description

--- pages/index.js
+++ pages/index.js
@@ -53,8 +53,9 @@ export default function Home() {
                             Procurement & Creative Marketer
                         </h3>
                         <p className="text-md py-5 leading-8 text-gray-700 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-                            Full Stack developer who uses code to provide services for business
-                            needs. Join me! and lets build fantastic web2 & web3 apps together.
+                        Providing marketing services with expertise in procurements of goods and market strategy formulation
+                        Join me lets brand and Improve your business while leveraging on new technology
+                    
                         </p>
                         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
                             <a href="https://twitter.com/dhe_plot">
@@ -77,16 +78,14 @@ export default function Home() {
                     <div>
                         <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
                         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                            Since the beginning of my journey as a freelance designer and developer,
-                            I've done remote work for
+                        WE ARE A FULL LUXURY SERVICE PROCUREMENT AND CREATIVE MARKETING COMPANY
+                        OUR SERVICE SPANNING - EXPERIENTIAL MARKETING, DIGITAL MARKETING, PROCUREMENT OF PROMOTIONAL MATERIALS AND ADVISORY SERVICES IN MARKETING AND PROmotions We hAve worked with 
                             <span className="text-teal-500"> agencies </span>
                             consulted for <span className="text-teal-500">startups </span>
-                            and collaborated with talanted people to create digital products for
+                            and collaborated with talanted people to create products for
                             both business and consumer use.
                         </p>
                         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                            I offer from a wide range of services, including brand design,
-                            programming and teaching.
                         </p>
                     </div>
                     <div className="md:flex gap-10">
