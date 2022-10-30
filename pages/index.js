d819ff6db33816392b9bf9db1986d87cf7cc7682
dhe-plot <115636889+dhe-plot@users.noreply.github.com>
2022-10-30 21:49:19 +0100

    New changes

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
                     <div>
                         <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
                         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                        I run a procurement and creative marketing company with our services spanning - Procurement of materials, Experiencial marketing, Digital marketing, Advisory services in marketing and Promotions. We hAve worked with 
+                        I run a procurement and creative marketing company with our services spanning - Procurement of Materials, Experiencial Marketing, Digital Marketing, Advisory Services in Marketing and Promotions. We have worked with 
                             <span className="text-teal-500"> agencies </span>
                             consulted for <span className="text-teal-500">startups </span>
                             and collaborated with talanted people to create products for
